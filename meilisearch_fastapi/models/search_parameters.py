65fe2944418b1783e7fb2e4c05bfb118306adb35
Paul Sanders <psanders1@gmail.com>
2021-07-09 21:36:24 -0400

    Updating types to work with python 3.7 and 3.8

--- meilisearch_fastapi/models/search_parameters.py
+++ meilisearch_fastapi/models/search_parameters.py
@@ -8,7 +8,7 @@ class SearchParameters(CamelBase):
     query: str
     offset: int = 0
     limit: int = 20
-    filter: Optional[Union[str, list[Union[str, list[str]]]]] = None
+    filter: Optional[Union[str, List[Union[str, List[str]]]]] = None
     facets_distribution: Optional[List[str]] = None
     attributes_to_retrieve: List[str] = ["*"]
     attributes_to_crop: Optional[List[str]] = None
