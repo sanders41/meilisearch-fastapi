12429dcbce503524925dcf95d4602842a70def8c
Paul Sanders <psanders1@gmail.com>
2021-08-03 18:38:24 -0400

    Updating test

--- tests/test_settings_routes.py
+++ tests/test_settings_routes.py
@@ -23,13 +23,13 @@ async def test_settings_get(default_settings, index_uid, indexes_sample, test_cl
 
 
 @pytest.mark.asyncio
-async def test_settings_update_and_delete(default_settings, index_uid, indexes_sample, test_client):
+async def test_settings_update_and_delete(default_settings, index_uid, test_client):
     update_settings = {
         "uid": index_uid,
         "synonyms": {"wolverine": ["logan", "xmen"], "logan": ["wolverine", "xmen"]},
         "stopWords": ["stop", "words"],
         "rankingRules": ["words", "typo", "proximity"],
-        "filterableAttributes": ["attributes", "for", "faceting"],
+        "filterableAttributes": ["attributes", "filterable"],
         "distinctAttribute": "movie_id",
         "searchableAttributes": ["description", "title"],
         "displayedAttributes": ["genre", "title"],
@@ -39,9 +39,15 @@ async def test_settings_update_and_delete(default_settings, index_uid, indexes_s
     assert response.status_code == 200
 
     response = await test_client.get(f"/settings/{index_uid}")
-    update_settings.pop("uid")
 
     assert response.status_code == 200
+
+    returned_settings = response.json()
+
+    # Filterable attributes come back in random order so sort them to be able to compare
+    returned_settings["filterableAttributes"] = returned_settings["filterableAttributes"].sort()
+    update_settings.pop("uid")
+
     assert response.json() == update_settings
 
     response = await test_client.delete(f"/settings/{index_uid}")
@@ -51,4 +57,8 @@ async def test_settings_update_and_delete(default_settings, index_uid, indexes_s
     response = await test_client.get(f"/settings/{index_uid}")
 
     assert response.status_code == 200
+    returned_settings = response.json()
+
+    # Filterable attributes come back in random order so sort them to be able to compare
+    returned_settings["filterableAttributes"] = returned_settings["filterableAttributes"].sort()
     assert response.json() == default_settings
