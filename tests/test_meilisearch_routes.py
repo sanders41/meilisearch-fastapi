497f3f682b4471891681f204e8acf95088193fdd
Paul Sanders <psanders1@gmail.com>
2021-12-24 18:58:41 -0500

    Add routes for new auth keys API

--- tests/test_meilisearch_routes.py
+++ tests/test_meilisearch_routes.py
@@ -1,4 +1,37 @@
+from datetime import datetime, timedelta
+
 import pytest
+from meilisearch_python_async.errors import MeiliSearchApiError
+from meilisearch_python_async.models.client import KeyCreate
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def test_key(raw_client):
+    key_info = KeyCreate(description="test", actions=["search"], indexes=["movies"])
+    key = await raw_client.create_key(key_info)
+
+    yield key
+
+    try:
+        await raw_client.delete_key(key.key)
+    except MeiliSearchApiError:
+        pass
+
+
+@pytest.fixture
+@pytest.mark.asyncio
+async def test_key_info(raw_client):
+    key_info = KeyCreate(description="test", actions=["search"], indexes=["movies"])
+
+    yield key_info
+
+    try:
+        keys = await raw_client.get_keys()
+        key = next(x for x in keys if x.description == key_info.description)
+        await raw_client.delete_key(key.key)
+    except MeiliSearchApiError:
+        pass
 
 
 @pytest.mark.asyncio
@@ -7,14 +40,71 @@ async def test_get_health(test_client):
     assert response.json() == {"status": "available"}
 
 
-@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "expires_at",
+    (
+        None,
+        (datetime.utcnow() + timedelta(days=2)).isoformat(),
+    ),
+)
+@pytest.mark.asyncio
+async def test_create_key(expires_at, test_client, test_key_info):
+    if expires_at:
+        test_key_info.expires_at = expires_at
+
+    key = await test_client.post("/meilisearch/keys", json=test_key_info.dict())
+    key_info = key.json()
+
+    assert key_info["description"] == test_key_info.description
+    assert key_info["actions"] == test_key_info.actions
+    assert key_info["indexes"] == test_key_info.indexes
+
+    if expires_at:
+        assert key_info["expiresAt"].split("+")[0] == expires_at.split(".")[0]
+    else:
+        assert key_info["expiresAt"] is None
+
+
+@pytest.mark.asyncio
+async def test_delete_key(test_key, test_client, raw_client):
+    result = await test_client.delete(f"/meilisearch/keys/{test_key.key}")
+    assert result.status_code == 204
+
+    with pytest.raises(MeiliSearchApiError):
+        await raw_client.get_key(test_key.key)
+
+
 @pytest.mark.asyncio
 async def test_get_keys(test_client):
     response = await test_client.get("/meilisearch/keys")
 
     assert response.status_code == 200
-    assert "public" in response.json()
-    assert "private" in response.json()
+    assert len(response.json()) == 2
+
+
+@pytest.mark.asyncio
+async def test_get_key(test_key, test_client):
+    response = await test_client.get(f"/meilisearch/keys/{test_key.key}")
+    assert response.json()["description"] == test_key.description
+
+
+@pytest.mark.asyncio
+async def test_update_key(test_key, test_client):
+    update_key_info = {
+        "key": test_key.key,
+        "description": "updated",
+        "actions": ["*"],
+        "indexes": ["*"],
+        "expires_at": (datetime.utcnow() + timedelta(days=2)).isoformat(),
+    }
+
+    key = await test_client.patch(f"meilisearch/keys/{test_key.key}", json=update_key_info)
+    key_info = key.json()
+
+    assert key_info["description"] == update_key_info["description"]
+    assert key_info["actions"] == update_key_info["actions"]
+    assert key_info["indexes"] == update_key_info["indexes"]
+    assert key_info["expiresAt"].split("+")[0] == update_key_info["expires_at"].split(".")[0]
 
 
 @pytest.mark.asyncio
